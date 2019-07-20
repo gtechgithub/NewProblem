1156bb6c3e974f5cbe9698dc0a9e4297b764a8cc
gtechgithub <36880236+gtechgithub@users.noreply.github.com>
2019-07-20 10:00:54 +0800

    Update Fill.java

--- Fill.java
+++ Fill.java
@@ -39,6 +39,21 @@ public class Fill extends CanvasCommands{
 		}
 	}
 	
+	private boolean validateForBoundary(int row, int column, String fillColor) {
+	
+		//check if x is < canvasDecorator.getWidth()  and y < heigth and x >= 1 and y >=1, 
+		// and also check if canvasDecorator.getConvasValues()[row,column] != "X" if so add in the queue
+    	
+	   	if ( (row < canvasDecorator.getHeight() +1) && (column < canvasDecorator.getWidth()+1) && row >= 1 && column >= 1 
+    			&& ( (null == canvasDecorator.getConvasValues()[row] [column]) ||
+    			(null != canvasDecorator.getConvasValues()[row] [column] &&  !canvasDecorator.getConvasValues()[row] [column].equals("X")
+    			&& !canvasDecorator.getConvasValues()[row] [column].equals(fillColor) )) ) {
+	   		return true;
+	   	}
+	   	
+	   	return false;
+	}
+	
 	@Override
 	public void fill(int startCol, int startRow, String fillColor) {
 	
@@ -54,45 +69,27 @@ public class Fill extends CanvasCommands{
             //set  canvasDecorator.getConvasValues()[row][column] with O
         	canvasDecorator.getConvasValues()[rowColumn.getRow()][rowColumn.getCol()] = fillColor;
         	
-            //x-1, y , check if x is < canvasDecorator.getWidth()  and y < heigth and x >= 1 and y >=1, and also check if canvasDecorator.getConvasValues()[row,column] != "X" if so add in the queue
-        	if ( (rowColumn.getRow() -1) < canvasDecorator.getHeight() +1 && rowColumn.getCol() < canvasDecorator.getWidth()+1 && (rowColumn.getRow() -1) >= 1 && rowColumn.getCol() >= 1 
-        			&& ( (null == canvasDecorator.getConvasValues()[(rowColumn.getRow() -1)] [rowColumn.getCol()]) ||
-        			(null != canvasDecorator.getConvasValues()[(rowColumn.getRow() -1)] [rowColumn.getCol()] &&  !canvasDecorator.getConvasValues()[(rowColumn.getRow() -1)] [rowColumn.getCol()].equals("X")
-        			&& !canvasDecorator.getConvasValues()[rowColumn.getRow()-1] [rowColumn.getCol() ].equals(fillColor) )) ) {
-        		
+            //x-1, y , check if x is < canvasDecorator.getWidth()  and y < heigth and x >= 1 and y >=1,
+        	//and also check if canvasDecorator.getConvasValues()[row,column] != "X" if so add in the queue
+
+        	if ( validateForBoundary((rowColumn.getRow() -1), rowColumn.getCol(), fillColor)) {
         		checkAndAddInQueue(rowColumn.getRow() -1, rowColumn.getCol());
-        		
         	}
         	
         	// x+1, y, check as above
-        	if ( (rowColumn.getRow() + 1) < canvasDecorator.getHeight()+1 && rowColumn.getCol() < canvasDecorator.getWidth()+1 && (rowColumn.getRow() +1) >= 1 && rowColumn.getCol() >= 1 
-        			&& ( (null == canvasDecorator.getConvasValues()[(rowColumn.getRow() +1)] [rowColumn.getCol()]) ||
-        			( null != canvasDecorator.getConvasValues()[(rowColumn.getRow() + 1)] [rowColumn.getCol()]  && !canvasDecorator.getConvasValues()[(rowColumn.getRow() + 1)] [rowColumn.getCol()].equals("X")
-        			&& !canvasDecorator.getConvasValues()[rowColumn.getRow()+1] [rowColumn.getCol() ].equals(fillColor) )) ) {
-
+        	if ( validateForBoundary(rowColumn.getRow() + 1, rowColumn.getCol(),fillColor)) {
         		checkAndAddInQueue(rowColumn.getRow() + 1, rowColumn.getCol());
 
         	}
 
         	//x, y-1, check as above
-        	if ( rowColumn.getRow() < canvasDecorator.getHeight()+1 && (rowColumn.getCol() -1) < canvasDecorator.getWidth()+1 && rowColumn.getRow() >= 1 && ( rowColumn.getCol() -1 ) >= 1
-        			&& ( (null == canvasDecorator.getConvasValues()[(rowColumn.getRow())] [rowColumn.getCol() -1]) ||
-        			( null != canvasDecorator.getConvasValues()[rowColumn.getRow()] [rowColumn.getCol() -1 ]   && !canvasDecorator.getConvasValues()[rowColumn.getRow()] [rowColumn.getCol() -1 ].equals("X") 
-        			 && !canvasDecorator.getConvasValues()[rowColumn.getRow()] [rowColumn.getCol() -1 ].equals(fillColor) )) ) {
-        		
+        	if ( validateForBoundary(rowColumn.getRow(), rowColumn.getCol() -1,fillColor)) {
         		checkAndAddInQueue(rowColumn.getRow(), rowColumn.getCol() -1);
-
         	}
 
         	//x, y+1, check as above
-        	if ( rowColumn.getRow() < canvasDecorator.getHeight()+1 && (rowColumn.getCol() +1) < canvasDecorator.getWidth()+1 && rowColumn.getRow() >= 1 && ( rowColumn.getCol() +1 ) >= 1
-        			&& ( (null == canvasDecorator.getConvasValues()[(rowColumn.getRow())] [rowColumn.getCol() +1]) ||
-        			( null != canvasDecorator.getConvasValues()[rowColumn.getRow()] [rowColumn.getCol() +1 ] && !canvasDecorator.getConvasValues()[rowColumn.getRow()] [rowColumn.getCol() +1 ].equals("X")
-        			&& !canvasDecorator.getConvasValues()[rowColumn.getRow()] [rowColumn.getCol() + 1 ].equals(fillColor) )) ) {
-        		
-
+        	if ( validateForBoundary(rowColumn.getRow(), rowColumn.getCol() +1,fillColor)) {        		
         		checkAndAddInQueue(rowColumn.getRow(), rowColumn.getCol() + 1);
-
         	}
         }
 		
